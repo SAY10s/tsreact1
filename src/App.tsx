6645ee4bc37738681f1a0fc42d6746e606b4c4e4
Nikodem Majzner <nikodemmajzner@gmail.com>
2023-04-06 14:20:50 +0200

    ADD: addTodoHandler (working with function as a prop)

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
+import NewTodo from "./components/NewTodo";
 import Todos from "./components/Todos";
 import Todo from "./models/todo";
 
 function App() {
   const todos = [new Todo("Dummy text1"), new Todo("Second dummy")];
 
+  const addTodoHandler = (todoText: string) => {};
+
   return (
     <div>
+      <NewTodo onAddTodo={addTodoHandler} />
       <Todos items={todos} />
     </div>
   );
