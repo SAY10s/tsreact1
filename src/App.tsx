99534ea0f994a122abfb1183cbef3f4bde85f32a
Nikodem Majzner <nikodemmajzner@gmail.com>
2023-04-06 13:26:52 +0200

    ADD: data model

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
-import "./App.css";
+import Todos from "./components/Todos";
+import Todo from "./models/todo";
 
 function App() {
-  return <div></div>;
+  const todos = [new Todo("Dummy text1"), new Todo("Second dummy")];
+
+  return (
+    <div>
+      <Todos items={todos} />
+    </div>
+  );
 }
 
 export default App;
