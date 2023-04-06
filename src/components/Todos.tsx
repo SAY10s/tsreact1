6644a8fdf68cdf0f50e5c2ecde542d80296c81c4
Nikodem Majzner <nikodemmajzner@gmail.com>
2023-04-06 14:00:10 +0200

    EXERCISE: ADD: outsource TodoUI to new component

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Todo from "../models/todo";
+import TodoUI from "./TodoUI";
 
 const Todos: React.FC<{ items: Todo[] }> = (props) => {
   return (
     <ul>
       {props.items.map((item) => (
-        <li key={item.id}>{item.text}</li>
+        <TodoUI key={item.id} text={item.text} />
       ))}
     </ul>
   );
