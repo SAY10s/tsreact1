58f9c5365a4ed6bf4cd2a26e2e7d292b452b0a75
Nikodem Majzner <nikodemmajzner@gmail.com>
2023-04-06 16:33:00 +0200

    ADD: 2 separate sections

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import Todos from "./components/Todos";
 import Todo from "./models/todo";
+import { useState } from "react";
 
 function App() {
-  const todos = [new Todo("Dummy text1"), new Todo("Second dummy")];
+  const [toDos, setToDos] = useState<Todo[][]>([
+    [new Todo("Dummy text1"), new Todo("Second dummy")],
+    [new Todo("Dummy text1"), new Todo("Second dummy")],
+  ]);
 
   return (
     <div>
-      <Todos items={todos} />
+      <Todos items={toDos} />
     </div>
   );
 }
--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,13 +1,20 @@
 import React from "react";
 import Todo from "../models/todo";
 import TodoUI from "./TodoUI";
+import Classes from "./todos.module.css";
 
-const Todos: React.FC<{ items: Todo[] }> = (props) => {
+const Todos: React.FC<{ items: Todo[][] }> = (props) => {
   return (
     <ul>
-      {props.items.map((item) => (
-        <TodoUI key={item.id} text={item.text} />
-      ))}
+      {props.items.map((item) => {
+        return (
+          <div className={Classes.xd}>
+            {item.map((item2) => {
+              return <TodoUI key={item2.id} text={item2.text} />;
+            })}
+          </div>
+        );
+      })}
     </ul>
   );
 };
