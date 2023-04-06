9a69c28a8e2eb54bea54bb7943a5bf0cd57d1a58
Nikodem Majzner <nikodemmajzner@gmail.com>
2023-04-06 14:45:22 +0200

    ADD: implemented CSS

--- src/components/TodoUI.tsx
+++ src/components/TodoUI.tsx
@@ -1,5 +1,7 @@
+import Classes from "./TodoItem.module.css";
+
 const ToDoUI: React.FC<{ text: string }> = (props) => {
-  return <li>{props.text}</li>;
+  return <li className={Classes.item}>{props.text}</li>;
 };
 
 export default ToDoUI;
--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Todo from "../models/todo";
 import TodoUI from "./TodoUI";
+import Classes from "./Todos.module.css";
 
 const Todos: React.FC<{ items: Todo[] }> = (props) => {
   return (
-    <ul>
+    <ul className={Classes.todos}>
       {props.items.map((item) => (
         <TodoUI key={item.id} text={item.text} />
       ))}
